b90df05ba3b30940ac13d2f8043dcd3c81f7213e
splincode <omaxphp@yandex.com>
2020-11-19 21:34:09 +0200

    Added Services

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,13 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Hero } from '../hero';
+
 
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
   styleUrls: ['./hero-detail.component.scss']
 })
+
+
 export class HeroDetailComponent implements OnInit {
 
-  constructor() { }
+  @Input() hero: Hero | null;
+
+  constructor() { 
+    this.hero = null;
+  }
 
   ngOnInit(): void {
   }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
  
 @Component({
   selector: 'app-heroes',
@@ -10,18 +11,26 @@ import { HEROES } from '../mock-heroes';
 
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;  
+  heroes: Hero[];
   selectedHero: Hero | null;
 
-  constructor() { 
-    this.selectedHero = null;
+  constructor(private heroService: HeroService, private messageService: MessageService) { 
+
+    this.selectedHero = null;      
+    this.heroes = [];
   }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.getHeroes();
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
+  getHeroes(): void {
+    this.heroService.getHeroes() 
+    .subscribe(heroes => this.heroes = heroes);
+  }
 }
