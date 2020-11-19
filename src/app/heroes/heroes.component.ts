752a5fc44a168bcd6b96a33c63cd06d0403b44ae
splincode <omaxphp@yandex.com>
2020-11-19 18:12:18 +0200

    added Display a List

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,20 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
  
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
-export class HeroesComponent implements OnInit {
-    hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  } 
 
-  constructor() { }
+export class HeroesComponent implements OnInit {
+
+  heroes = HEROES;  
+  selectedHero: Hero | null;
+
+  constructor() { 
+    this.selectedHero = null;
+  }
 
   ngOnInit(): void {
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
