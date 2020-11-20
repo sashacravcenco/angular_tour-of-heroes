41c0f207d6e1930133ad28ee634b23602aa2ce03
splincode <omaxphp@yandex.com>
2020-11-20 22:38:55 +0200

    Got Data from a Server

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,26 +1,23 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Hero } from '../hero';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
-import { HeroService } from '../hero.service';
 
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.scss']
+  styleUrls: [ './hero-detail.component.scss' ]
 })
-
-
 export class HeroDetailComponent implements OnInit {
-
   hero: Hero;
 
   constructor(
     private route: ActivatedRoute,
     private heroService: HeroService,
     private location: Location
-  ) {  }
+  ) {}
 
   ngOnInit(): void {
     this.getHero();
@@ -30,10 +27,14 @@ export class HeroDetailComponent implements OnInit {
     const id = this.route.snapshot.paramMap.get('id');
     this.heroService.getHero(Number(id))
       .subscribe(hero => this.hero = hero);
-}
+  }
 
   goBack(): void {
     this.location.back();
   }
 
+  save(): void {
+    this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
+  }
 }
